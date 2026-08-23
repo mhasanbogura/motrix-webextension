a3f9a8401d0ab99593bf03e05b2d25821a4f710a
mhasanbogura <1.16979039e+08+mhasanbogura@users.noreply.github.com>
2026-08-23 20:08:21 +0000

    Round picker outer corners

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
 
-const RELEASE_VERSION = '1.6.48';
+const RELEASE_VERSION = '1.6.49';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
